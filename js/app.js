e8e272b618160cd39f564c619285b86cc0af8162
avituo <a.vituo_@outlook.com>
2024-09-13 01:02:15 -0300

    Starting project

---
+++ js/app.js
@@ -0,0 +1,9 @@
+// URL da API
+const apiBaseUrl = 'http://localhost:8080';
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('helloWorldBtn').addEventListener('click', () => {
+        console.log('Hello World!');
+    });
+
+});
